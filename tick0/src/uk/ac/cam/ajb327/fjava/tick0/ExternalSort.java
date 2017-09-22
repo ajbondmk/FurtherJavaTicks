589d2c7c80cd66a98c4fa6b9a252eb455b7b9038
Aliyah Bond <ajbondmk@hotmail.co.uk>
2017-09-23 00:54:31 +0100

    Works with a single thread and a timeout

--- tick0/src/uk/ac/cam/ajb327/fjava/tick0/ExternalSort.java
+++ tick0/src/uk/ac/cam/ajb327/fjava/tick0/ExternalSort.java
@@ -8,18 +8,14 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.nio.channels.FileChannel;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class ExternalSort {
 
-	private static long timeSpentInitial = 0;
-	private static long timeSpentMerge = 0;
+	private static int timeSpentInitial = 0;
+	private static int timeSpentMerge = 0;
 
 	public static void sort(String f1, String f2) throws FileNotFoundException, IOException {
 
@@ -30,8 +26,29 @@ public class ExternalSort {
 
 		int initialSortInts = 65536;
 
-		initialSort(f1, f2, fileLength, initialSortInts);
-		mergeSort(f1, f2, fileLength, initialSortInts);
+		SortThread t1 = new SortThread("testThread");
+		t1.start(f1, f2, 0, fileLength, initialSortInts);
+
+		try {
+			t1.join();
+		} catch (Exception e) {
+			System.out.println("Interrupted");
+		}
+
+//		try {
+//			Thread.sleep(10000);
+//		} catch (InterruptedException e) {
+//
+//		}
+
+		timeSpentInitial += t1.timeSpentInitial;
+		timeSpentMerge += t1.timeSpentMerge;
+
+		System.out.println();
+		System.out.println("Back in main class");
+
+		//initialSort(f1, f2, fileLength, initialSortInts);
+		//mergeSort(f1, f2, fileLength, initialSortInts);
 
 	}
 
@@ -41,120 +58,6 @@ public class ExternalSort {
 
 	private static DataInputStream getInputStream(String location) throws IOException {
 		return new DataInputStream(new BufferedInputStream(new FileInputStream(location)));
-	}
-
-	private static void initialSort(String f1, String f2, int fileLength, int initialSortInts) throws IOException {
-
-		long startTime = System.nanoTime();
-
-		List<Integer> chunkToSort = new ArrayList<>();
-
-		DataInputStream dInInitial = getInputStream(f1);
-		DataOutputStream dOutInitial = getOutputStream(f2);
-
-		int fileLengthInts = fileLength / 4;
-
-		for (int num = 1; num <= fileLengthInts; num++) {
-			chunkToSort.add(dInInitial.readInt());
-			boolean endOfChunk = (num % initialSortInts == 0);
-			boolean endOfFile = (num == fileLengthInts);
-			if (endOfChunk || endOfFile) {
-				Collections.sort(chunkToSort);
-				for (Integer i : chunkToSort) {
-					dOutInitial.writeInt(i);
-				}
-				chunkToSort.clear();
-				if (endOfFile) break;
-			}
-		}
-
-		dOutInitial.flush();
-
-		dInInitial.close();
-		dOutInitial.close();
-
-		long endTime = System.nanoTime();
-		timeSpentInitial += endTime - startTime;
-
-	}
-
-	private static void mergeSort(String f1, String f2, int fileLength, int initialSortInts) throws IOException {
-
-		long startTime = System.nanoTime();
-
-		boolean readingFromF1 = false;
-
-		for (int chunkSize = initialSortInts * 4; chunkSize < fileLength; chunkSize *= 2) {
-
-			int lengthLeft = fileLength;
-
-			DataInputStream dIn1 = getInputStream(readingFromF1 ? f1 : f2);
-			DataInputStream dIn2 = getInputStream(readingFromF1 ? f1 : f2);
-			DataOutputStream dOut = getOutputStream(readingFromF1 ? f2 : f1);
-			dIn2.skip(chunkSize);
-
-			while (true) {
-
-				if (lengthLeft > chunkSize) {
-
-					int leftIn1 = chunkSize;
-					int leftIn2 = chunkSize;
-					if (lengthLeft < 2 * chunkSize) {
-						leftIn2 = lengthLeft - chunkSize;
-					}
-					int current1 = dIn1.readInt();
-					int current2 = dIn2.readInt();
-
-					while (leftIn1 > 0 || leftIn2 > 0) {
-						if ((current1 < current2 && leftIn1 > 0) || leftIn2 == 0) {
-							dOut.writeInt(current1);
-							leftIn1 -= 4;
-							if (leftIn1 > 0) current1 = dIn1.readInt();
-						} else {
-							dOut.writeInt(current2);
-							leftIn2 -= 4;
-							if (leftIn2 > 0) current2 = dIn2.readInt();
-						}
-					}
-
-					dIn1.skip(chunkSize);
-					dIn2.skip(chunkSize);
-					lengthLeft -= 2 * chunkSize;
-					if (lengthLeft < 0) lengthLeft = 0;
-
-				} else {
-
-					while (lengthLeft > 0) {
-						dOut.writeInt(dIn1.readInt());
-						lengthLeft -= 4;
-					}
-					break;
-
-				}
-
-			}
-
-			dOut.flush();
-
-			dIn1.close();
-			dIn2.close();
-			dOut.close();
-
-			readingFromF1 = !readingFromF1;
-
-		}
-
-		if (!readingFromF1) copyToF1(f1, f2, fileLength);
-
-		long endTime = System.nanoTime();
-		timeSpentMerge += endTime - startTime;
-
-	}
-
-	private static void copyToF1(String f1, String f2, int fileLength) throws IOException {
-		FileChannel src = new FileInputStream(f2).getChannel();
-		FileChannel dest = new FileOutputStream(f1).getChannel();
-		dest.transferFrom(src, 0, fileLength);
 	}
 
 	private static void printFile(DataInputStream dIn) {
@@ -212,6 +115,7 @@ public class ExternalSort {
 			String f2 = "test-suite/test" + testNum + "b.dat";
 			sort(f1, f2);
 			checkCheckSum(testNum, f1);
+			//printFile(getInputStream(f1));
 		}
 		long endTime = System.nanoTime();
 		long timeTaken = endTime - startTime;
---
+++ tick0/src/uk/ac/cam/ajb327/fjava/tick0/SortThread.java
@@ -0,0 +1,198 @@
+package uk.ac.cam.ajb327.fjava.tick0;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortThread extends Thread {
+
+	private Thread thread;
+	private String threadName;
+
+	private String f2;
+	private String f1;
+	private int startPos;
+	private int fileLength;
+	private int initialSortInts;
+
+	public static long timeSpentInitial = 0;
+	public static long timeSpentMerge = 0;
+
+	SortThread (String name) {
+		threadName = name;
+	}
+
+	public void run () {
+		try {
+			initialSort(f1, f2, fileLength, initialSortInts);
+			mergeSort(f1, f2, fileLength, initialSortInts);
+		} catch (IOException e) {
+			//TODO: REMOVE THIS
+			System.out.println("IO EXCEPTION HALP");
+		}
+	}
+
+	public void start (String F1, String F2, int StartPos, int FileLength, int InitialSortInts) {
+		f1 = F1;
+		f2 = F2;
+		startPos = StartPos;
+		fileLength = FileLength;
+		initialSortInts = InitialSortInts;
+		if (thread == null) {
+			thread = new Thread (this, threadName);
+			thread.start();
+		}
+	}
+
+	private DataOutputStream getOutputStream(String location) throws IOException {
+		RandomAccessFile f = new RandomAccessFile(location, "rw");
+		f.seek(startPos);
+		return new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f.getFD())));
+	}
+
+	private DataInputStream getInputStream(String location) throws IOException {
+		DataInputStream dIn = new DataInputStream(new BufferedInputStream(new FileInputStream(location)));
+		dIn.skip(startPos);
+		return dIn;
+	}
+
+	private void initialSort(String f1, String f2, int fileLength, int initialSortInts) throws IOException {
+
+		long startTime = System.nanoTime();
+
+		List<Integer> chunkToSort = new ArrayList<>();
+
+		DataInputStream dInInitial = getInputStream(f1);
+		DataOutputStream dOutInitial = getOutputStream(f2);
+
+		int fileLengthInts = fileLength / 4;
+
+		for (int num = 1; num <= fileLengthInts; num++) {
+			chunkToSort.add(dInInitial.readInt());
+			boolean endOfChunk = (num % initialSortInts == 0);
+			boolean endOfFile = (num == fileLengthInts);
+			if (endOfChunk || endOfFile) {
+				Collections.sort(chunkToSort);
+				for (Integer i : chunkToSort) {
+					dOutInitial.writeInt(i);
+				}
+				chunkToSort.clear();
+				if (endOfFile) break;
+			}
+		}
+
+		dOutInitial.flush();
+
+		dInInitial.close();
+		dOutInitial.close();
+
+		long endTime = System.nanoTime();
+		timeSpentInitial += endTime - startTime;
+
+		System.out.println("End of initial sort");
+
+	}
+
+	private static void printFile(String f) {
+		try {
+			DataInputStream dIn = new DataInputStream(new BufferedInputStream(new FileInputStream(f)));
+			while (dIn.available() > 0) {
+				System.out.print(dIn.readInt() + " ");
+			}
+			System.out.println();
+		} catch (IOException err) {
+			System.out.println(err.getMessage());
+		}
+	}
+
+	private void mergeSort(String f1, String f2, int fileLength, int initialSortInts) throws IOException {
+
+		long startTime = System.nanoTime();
+
+		boolean readingFromF1 = false;
+
+		for (int chunkSize = initialSortInts * 4; chunkSize < fileLength; chunkSize *= 2) {
+
+			int lengthLeft = fileLength;
+
+			DataInputStream dIn1 = getInputStream(readingFromF1 ? f1 : f2);
+			DataInputStream dIn2 = getInputStream(readingFromF1 ? f1 : f2);
+			DataOutputStream dOut = getOutputStream(readingFromF1 ? f2 : f1);
+			dIn2.skip(chunkSize);
+
+			while (true) {
+
+				if (lengthLeft > chunkSize) {
+
+					int leftIn1 = chunkSize;
+					int leftIn2 = chunkSize;
+					if (lengthLeft < 2 * chunkSize) {
+						leftIn2 = lengthLeft - chunkSize;
+					}
+					int current1 = dIn1.readInt();
+					int current2 = dIn2.readInt();
+
+					while (leftIn1 > 0 || leftIn2 > 0) {
+						if ((current1 < current2 && leftIn1 > 0) || leftIn2 == 0) {
+							dOut.writeInt(current1);
+							leftIn1 -= 4;
+							if (leftIn1 > 0) current1 = dIn1.readInt();
+						} else {
+							dOut.writeInt(current2);
+							leftIn2 -= 4;
+							if (leftIn2 > 0) current2 = dIn2.readInt();
+						}
+					}
+
+					dIn1.skip(chunkSize);
+					dIn2.skip(chunkSize);
+					lengthLeft -= 2 * chunkSize;
+					if (lengthLeft < 0) lengthLeft = 0;
+
+				} else {
+
+					while (lengthLeft > 0) {
+						dOut.writeInt(dIn1.readInt());
+						lengthLeft -= 4;
+					}
+					break;
+
+				}
+
+			}
+
+			dOut.flush();
+
+			dIn1.close();
+			dIn2.close();
+			dOut.close();
+
+			readingFromF1 = !readingFromF1;
+
+		}
+
+		if (!readingFromF1) copyToF1(f1, f2, fileLength);
+
+		long endTime = System.nanoTime();
+		timeSpentMerge += endTime - startTime;
+
+		System.out.println("End of merge sort");
+
+	}
+
+	private static void copyToF1(String f1, String f2, int fileLength) throws IOException {
+		FileChannel src = new FileInputStream(f2).getChannel();
+		FileChannel dest = new FileOutputStream(f1).getChannel();
+		dest.transferFrom(src, 0, fileLength);
+	}
+
+}
